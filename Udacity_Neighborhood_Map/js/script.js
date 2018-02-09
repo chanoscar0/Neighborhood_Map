4ed000bcd210c9aa8c4fecba9a7f6264fa7f46ab
Oscar Chan <chanoscar0@gmail.com>
2018-02-09 09:58:53 -0800

    Set-up Filters for different cities

--- Udacity_Neighborhood_Map/js/script.js
+++ Udacity_Neighborhood_Map/js/script.js
@@ -1,14 +1,14 @@
 var map;
 var markers = [];
 var locations = [
-  {title: 'Disneyland Park', location: {lat: 33.812511, lng: -117.918976}},
-  {title: 'Universal Studios', location: {lat: 34.1381, lng: -118.3534}},
-  {title: 'Angel Stadium', location: {lat: 33.8366, lng: -117.9143}},
-  {title: 'Dodgers Stadium', location: {lat: 34.0739, lng: -118.2400}},
-  {title: 'Staples Center', location: {lat: 34.0430, lng: -118.2673}},
-  {title: 'Fashion Island', location: {lat: 33.6159, lng: -117.8758}},
-  {title: 'SeaWorld', location: {lat: 33.468986, lng: -117.673389}},
-  {title: 'LegoLand', location: {lat: 33.126205, lng: -117.311606}}
+  {title: 'Disneyland Park', location: {lat: 33.812511, lng: -117.918976}, city: 'Anaheim'},
+  {title: 'Universal Studios', location: {lat: 34.1381, lng: -118.3534}, city: 'Los Angeles'},
+  {title: 'Angel Stadium', location: {lat: 33.8366, lng: -117.9143}, city: 'Anaheim'},
+  {title: 'Dodgers Stadium', location: {lat: 34.0739, lng: -118.2400}, city: 'Los Angeles'},
+  {title: 'Staples Center', location: {lat: 34.0430, lng: -118.2673}, city: 'Los Angeles'},
+  {title: 'Fashion Island', location: {lat: 33.6159, lng: -117.8758}, city: 'Newport Beach'},
+  {title: 'SeaWorld', location: {lat: 33.468986, lng: -117.673389}, city: 'San Diego'},
+  {title: 'LegoLand', location: {lat: 33.126205, lng: -117.311606}, city: 'San Diego'}
 ];
 function initMap(){
   map = new google.maps.Map(document.getElementById('googleMap'), {
@@ -38,19 +38,41 @@ function initMap(){
 var Location = function(data) {
   this.title = ko.observable(data.title);
   this.location = ko.observable(data.location);
+  this.city = ko.observable(data.city);
 }
 var ViewModel = function(){
   var self = this;
-  this.locationList = ko.observableArray([]);
+  //Filter options in the drop-down menu
+  self.filterOptions = ['None','Newport Beach','Anaheim','Los Angeles','San Diego'];
+
+  //Observable to hold selected input
+  self.selectedFilter = ko.observable('');
+  self.locationList = ko.observableArray([]);
     locations.forEach(function(locationItem){
       self.locationList.push(new Location(locationItem));
     });
+    //Computation for deciding which locations to show based on the filters
+  self.filteredItems = ko.computed(function(){
+    var filter = self.selectedFilter();
+    if(!filter || filter == "None"){
+      return self.locationList()
+    } else{
+      return ko.utils.arrayFilter(self.locationList(), function(i){
+        return i.city() == filter;
+      });
+    }
+  });
 
     this.currentLocation = ko.observable(this.locationList()[0]);
 
     self.setCurrentLocation = function(clickedLocation){
       self.currentLocation(clickedLocation);
     };
+    self.currentFilter = ko.observable(this.filterOptions[0]);
 
+    self.setcurrentFilter = function(clickedFilter){
+      self.currentFilter(clickedFilter);
+    }
+    console.log(this.selectedFilter());
 }
 ko.applyBindings(new ViewModel());
