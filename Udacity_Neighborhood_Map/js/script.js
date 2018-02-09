9f0729428cda7bcce8fcd199869562b7ae901818
Oscar Chan <chanoscar0@gmail.com>
2018-02-09 11:26:16 -0800

    Rearranged ViewModel to incorporate Google Maps API functions

--- Udacity_Neighborhood_Map/js/script.js
+++ Udacity_Neighborhood_Map/js/script.js
@@ -1,5 +1,4 @@
-var map;
-var markers = [];
+
 var locations = [
   {title: 'Disneyland Park', location: {lat: 33.812511, lng: -117.918976}, city: 'Anaheim'},
   {title: 'Universal Studios', location: {lat: 34.1381, lng: -118.3534}, city: 'Los Angeles'},
@@ -10,69 +9,69 @@ var locations = [
   {title: 'SeaWorld', location: {lat: 33.468986, lng: -117.673389}, city: 'San Diego'},
   {title: 'LegoLand', location: {lat: 33.126205, lng: -117.311606}, city: 'San Diego'}
 ];
-function initMap(){
-  map = new google.maps.Map(document.getElementById('googleMap'), {
-    center: {lat: 33.8366, lng:-117.9143},
-    zoom: 13
-  });
-  var bounds = new google.maps.LatLngBounds();
-
-  for (var i = 0; i < locations.length; i++) {
-    // Get the position from the location array.
-    var position = locations[i].location;
-    var title = locations[i].title;
-    // Create a marker per location, and put into markers array.
-    var marker = new google.maps.Marker({
-      map: map,
-      position: position,
-      title: title,
-      animation: google.maps.Animation.DROP,
-      id: i
-    });
-    markers.push(marker);
-    bounds.extend(markers[i].position);
-  }
-  map.fitBounds(bounds);
-
-}
-var Location = function(data) {
-  this.title = ko.observable(data.title);
-  this.location = ko.observable(data.location);
-  this.city = ko.observable(data.city);
-}
 var ViewModel = function(){
+  var map;
+  var markers = [];
   var self = this;
-  //Filter options in the drop-down menu
   self.filterOptions = ['None','Newport Beach','Anaheim','Los Angeles','San Diego'];
+  self.currentFilter = ko.observable(this.filterOptions[0]);
 
   //Observable to hold selected input
   self.selectedFilter = ko.observable('');
   self.locationList = ko.observableArray([]);
     locations.forEach(function(locationItem){
-      self.locationList.push(new Location(locationItem));
+      self.locationList.push(locationItem);
     });
-    //Computation for deciding which locations to show based on the filters
+  this.currentLocation = ko.observable(this.locationList()[0]);
+  ViewModel.prototype.initMap = function(){
+      map = new google.maps.Map(document.getElementById('googleMap'), {
+        center: {lat: 33.8366, lng:-117.9143},
+        zoom: 13
+      });
+      var bounds = new google.maps.LatLngBounds();
+
+      for (var i = 0; i < locations.length; i++) {
+        // Get the position from the location array.
+        var position = locations[i].location;
+        var title = locations[i].title;
+        var city = locations[i].city;
+        // Create a marker per location, and put into markers array.
+        var marker = new google.maps.Marker({
+          map: map,
+          position: position,
+          title: title,
+          category: city,
+          animation: google.maps.Animation.DROP,
+          id: i
+        });
+        markers.push(marker);
+        bounds.extend(markers[i].position);
+
+      }
+      map.fitBounds(bounds);
+
+    }
+
   self.filteredItems = ko.computed(function(){
     var filter = self.selectedFilter();
     if(!filter || filter == "None"){
       return self.locationList()
     } else{
       return ko.utils.arrayFilter(self.locationList(), function(i){
-        return i.city() == filter;
+        return i.city == filter;
       });
     }
   });
 
-    this.currentLocation = ko.observable(this.locationList()[0]);
-
     self.setCurrentLocation = function(clickedLocation){
       self.currentLocation(clickedLocation);
+
     };
-    self.currentFilter = ko.observable(this.filterOptions[0]);
 
     self.setcurrentFilter = function(clickedFilter){
       self.currentFilter(clickedFilter);
     }
-    console.log(this.selectedFilter());
 }
-ko.applyBindings(new ViewModel());
+var VM = new ViewModel();
+
+ko.applyBindings(VM);
