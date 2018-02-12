fd047c30ece633d90c412d0be9d956a9332b58b1
Oscar Chan <chanoscar0@gmail.com>
2018-02-12 09:13:19 -0800

    Inputting final changes to app

--- Udacity_Neighborhood_Map/js/script.js
+++ Udacity_Neighborhood_Map/js/script.js
@@ -2,8 +2,7 @@
 var locations = [
   {title: 'Disneyland Park', location: {lat: 33.8121, lng: -117.9190}, city: 'Anaheim'},
   {title: 'Universal Studios', location: {lat: 34.138, lng: -118.353}, city: 'Los Angeles'},
-  {title: 'Angel Stadium', location: {lat: 33.80, lng: -117.88}, city: 'Anaheim'},
-  {title: 'Dodger Stadium', location: {lat: 34.0736, lng: -118.2406}, city: 'Los Angeles'},
+  {title: 'Angel Stadium', location: {lat: 33.80003, lng: -117.883043}, city: 'Anaheim'},
   {title: 'Staples Center', location: {lat: 34.0430, lng: -118.2673}, city: 'Los Angeles'},
   {title: 'Fashion Island', location: {lat: 33.6159, lng: -117.8758}, city: 'Newport Beach'},
   {title: 'SeaWorld', location: {lat: 32.7648, lng: -117.2266}, city: 'San Diego'},
@@ -22,6 +21,7 @@ var ViewModel = function(){
   self.currentFilter = ko.observable(this.filterOptions[0]);
   //Observable to hold selected input
   self.selectedFilter = ko.observable('');
+  //Add locations into the oberservable array locationlist
   self.locationList = ko.observableArray([]);
     locations.forEach(function(locationItem){
       self.locationList.push(locationItem);
@@ -87,9 +87,10 @@ var ViewModel = function(){
             lng = locations[i].location.lng;
           }
         }
-        var foursquareURLSearch = 'https://api.foursquare.com/v2/venues/search?' ;
+        var foursquareURLSearch = 'https://api.foursquare.com/v2/venues/search?';
         var clientID = 'WDOUKNQBEJPN0T1APND43KYKGEHI2SGPMG5RH3EUG4MDSA5O';
         var clientSecret = "5SNYDXL0BZNGWZP5XEQZGLWLCZ04FMTS3GNYS4HGLSGF0D54";
+        //outer Ajax method to find the location's address and information
         $.ajax({
           url: foursquareURLSearch,
           dataType: 'json',
@@ -100,14 +101,20 @@ var ViewModel = function(){
                 '&v=20170801' +
                 '&m=foursquare',
           async: true,
-
+          //If server is reachable, dissect the JSON response to the information we need
           success: function(data){
             venueID = data['response']['venues'][0].id;
             var result = data['response']['venues'][0];
             var locationName = result.name;
             var locationFormattedAddress = result.location.formattedAddress;
-            contentString = locationName + ' is located at ' + locationFormattedAddress[0]
-            +' ' + locationFormattedAddress[1] + ' ' + locationFormattedAddress[2];
+            contentString = locationName + ' is located at ' +
+            locationFormattedAddress[0] +' ' + locationFormattedAddress[1] +
+            ' ' + locationFormattedAddress[2];
+            contentString = "<div>" + contentString + "</div>";
+            /*Inner AJAX function that uses the venueID we get from the outer
+            function, to get the photos from the foursquare API. The response is
+            then processed, and used to populate the infoWindow
+            */
             $.ajax({
               url: "https://api.foursquare.com/v2/venues/" + venueID + "/photos?",
               dataType: 'json',
@@ -118,41 +125,49 @@ var ViewModel = function(){
               async: true,
 
               success: function(data){
-                var resultPhotos = data['response']['photos']['items'][0];
-                var prefix = resultPhotos.prefix;
-                var suffix = resultPhotos.suffix;
-                var width = resultPhotos.width;
-                var height = resultPhotos.height;
-                imageString = prefix + '100x100' + suffix;
-                if (infowindow.marker != marker) {
-                  infowindow.marker = marker;
-                  infowindow.maxWidth = 50;
-                  infowindow.setContent("<div style= 'float:left;'>" + "<img src = " + imageString + "></div><div style = 'content:center'>"  + contentString + '</div>' );
-                  infowindow.open(map, marker);
-                  // Make sure the marker property is cleared if the infowindow is closed.
-                  infowindow.addListener('closeclick', function() {
-                    infowindow.marker = null;
-                  });
+                if (data['response']['photos']['items'].length == 0){
+                  alert("Sorry, we could not load photo for this location.");
+                } else{
+                  var resultPhotos = data['response']['photos']['items'][0];
+                  var prefix = resultPhotos.prefix;
+                  var suffix = resultPhotos.suffix;
+                  var width = resultPhotos.width;
+                  var height = resultPhotos.height;
+                  imageString = prefix + '100x100' + suffix;
+                  imageString = "<div style = float:left;> <img src = " +
+                                imageString + "></div>";
+                  contentString = imageString + contentString;
+                  if (infowindow.marker != marker) {
+                    infowindow.marker = marker;
+                    infowindow.setContent(contentString);
+                    infowindow.open(map, marker);
+                    /* Make sure the marker property is cleared if the
+                    infowindow is closed.*/
+                    infowindow.addListener('closeclick', function() {
+                      infowindow.marker = null;
+                    });
                 }
               }
+              }
             });
-
           },
+          //If foursquare is not reachable, alert that an error has occurred.
           fail: function(response){
-            alert("Error, we could not load the fourSquare data for this location. ")
+            alert("Error, we could not load the fourSquare data for this location. ");
           }
       });
     }
-
+      //Function for showing all markers on a map on load.
       function showLocations() {
             // Extend the boundaries of the map for each marker and display the marker
             for (var i = 0; i < markers.length; i++) {
                 markers[i].setMap(map);
                 bounds.extend(markers[i].position);
             }
+            //fit map to the markers
             map.fitBounds(bounds);
       }
-
+      //Function for hiding markers on the map
       function hideLocations(){
         //Hide all the markers on the map
         for (var i=0; i <markers.length; i++) {
@@ -160,7 +175,7 @@ var ViewModel = function(){
         }
       }
       showLocations();
-
+      //Function used to make the customer markers with colors.
       function makeMarkerIcon(markerColor) {
         var markerImage = new google.maps.MarkerImage(
           'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
@@ -173,7 +188,7 @@ var ViewModel = function(){
       }
 
     }
-
+//computed function in knockout to display the filtered or unfiltered list
   self.filteredItems = ko.computed(function(){
     var filter = self.selectedFilter();
     if(!filter || filter == "None"){
@@ -184,6 +199,7 @@ var ViewModel = function(){
       });
     }
   });
+  //filter markers to match the filtered list
     self.filterMarkers = function(category){
       for (i = 0; i < markers.length; i++) {
         marker = markers[i];
@@ -198,11 +214,11 @@ var ViewModel = function(){
         }
     }
   }
-
+    //Set's current location
     self.setCurrentLocation = function(clickedLocation){
       self.currentLocation(clickedLocation);
       google.maps.event.trigger(clickedLocation.marker, 'click');
-}
+}   //Set's current filter
     self.setcurrentFilter = function(clickedFilter){
       self.currentFilter(clickedFilter);
     }
